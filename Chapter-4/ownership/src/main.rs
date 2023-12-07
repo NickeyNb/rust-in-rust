ccef5fb4b12e9dcf22ab29db74d3568c6f518b70
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-07 10:55:53 +0530

    4.2

--- Chapter-4/ownership/src/main.rs
+++ Chapter-4/ownership/src/main.rs
@@ -85,17 +85,83 @@
 // Rust does let us return multiple values using a tuple, as shown in Listing 4-5.
 
 
-fn main() {
-    let s1 = String::from("hello");
+// fn main() {
+//     let s1 = String::from("hello");
     
-    let (s2, len) = calculate_length(s1);
+//     let (s2, len) = calculate_length(s1);
     
-    println!("The length of '{}' is {}.", s2, len);
-}
+//     println!("The length of '{}' is {}.", s2, len);
+// }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len(); // len() returns the length of a String
     
-    (s, length)
-}
+//     (s, length)
+// }
 // Luckily for us, Rust has a feature for using a value without transferring ownership, called references.
+
+// ====================================
+// ---References and Borrowing---
+
+// fn main () {
+//     let s1=String::from("hello");
+//     let len=calculate_length(&s1);
+//     // The &s1 syntax lets us create a reference that refers to the value of s1 but does not own it. 
+//     // Because it does not own it, the value it points to will not be dropped when the reference stops being used.
+
+//     println!("s1 is {s1} and len is {len}");
+
+// }
+// // the signature of the function uses & to indicate that the type of the parameter s is a reference.
+// fn calculate_length(s:&String)->usize {
+//     s.len()
+// }   // Here, s goes out of scope. But because it does not have ownership of what
+//   // it refers to, it is not dropped.
+
+// ---Mutable References---
+// fn main() {
+//     let mut s1=String::from("hello");
+//     println!("s1 is {s1}"); // s1 is hello
+//     change(&mut s1); 
+//     println!("s1 is {s1}"); // s1 is hello, world
+
+// }
+// fn change(s:&mut String) {
+//     s.push_str(", world");
+// }
+// This makes it very clear that the change function will mutate the value it borrows.
+// Mutable references have one big restriction: if you have a mutable reference to a value, you can have no other references to that value.
+// let mut s = String::from("hello");
+// let r1 = &mut s;
+// let r2 = &mut s;
+
+// The benefit of having this restriction is that Rust can prevent data races at compile time. A data race is similar to a race condition and happens when these three behaviors occur:
+// Two or more pointers access the same data at the same time.
+// At least one of the pointers is being used to write to the data.
+// There’s no mechanism being used to synchronize access to the data.
+
+// let mut s = String::from("hello");
+// let r1 = &s; // no problem
+// let r2 = &s; // no problem
+// let r3 = &mut s; // BIG PROBLEM
+
+// Note that a reference’s scope starts from where it is introduced and continues through the last time that reference is used. For instance, this code will compile because the last usage of the immutable references, the println!, occurs before the mutable reference is introduced:
+
+    // let mut s = String::from("hello");
+
+    // let r1 = &s; // no problem
+    // let r2 = &s; // no problem
+    // println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    // let r3 = &mut s; // no problem
+    // println!("{}", r3);
+    // The scopes of the immutable references r1 and r2 end after the println! where they are last used, which is before the mutable reference r3 is created. These scopes don’t overlap, so this code is allowed: the compiler can tell that the reference is no longer being used at a point before the end of the scope.
+// ======================================
+
+// ---Dangling references---
+// In Rust, by contrast, the compiler guarantees that references will never be dangling references: if you have a reference to some data, the compiler will ensure that the data will not go out of scope before the reference to the data does
+
+// The Rules of References
+// At any given time, you can have either one mutable reference or any number of immutable references.
+// References must always be valid.
