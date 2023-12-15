63e1b4c66718ec6955ab9b6615989276660cbfc6
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-15 10:37:19 +0530

    deleted chapter-5 target

--- Chapter-5/structures/target/debug/deps/structures-e96f8c4c761b244f.d
+++
@@ -1,5 +0,0 @@
-/home/nitin/My-Folder/rust-in-rust/Chapter-5/structures/target/debug/deps/structures-e96f8c4c761b244f: src/main.rs
-
-/home/nitin/My-Folder/rust-in-rust/Chapter-5/structures/target/debug/deps/structures-e96f8c4c761b244f.d: src/main.rs
-
-src/main.rs:
--- Chapter-5/structures/target/debug/structures.d
+++
@@ -1,1 +0,0 @@
-/home/nitin/My-Folder/rust-in-rust/Chapter-5/structures/target/debug/structures: /home/nitin/My-Folder/rust-in-rust/Chapter-5/structures/src/main.rs
