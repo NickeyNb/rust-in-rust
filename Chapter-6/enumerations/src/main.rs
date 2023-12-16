d0dc239cd3bb5b4f6f8ed36b13955df28fba2910
Nitin Bisht <nitinnb31122002@gmail.com>
2023-12-16 09:01:20 +0530

    6.2

--- Chapter-6/enumerations/src/main.rs
+++ Chapter-6/enumerations/src/main.rs
@@ -1,16 +1,20 @@
 // The match Control Flow Construct
 // Rust has an extremely powerful control flow construct called match that allows you to compare a value against a series of patterns and then execute code based on which pattern matches. 
+// Matches in Rust are exhaustive: we must exhaust every last possibility in order for the code to be valid. 
 
+#[derive(Debug)]
+enum  UsStates {
+    Alaska,
+    Alabama,
+    
+}
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsStates),
 }
 
-fn main () {
-
-}
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
         Coin::Penny => {
@@ -19,6 +23,25 @@ fn value_in_cents(coin: Coin) -> u8 {
         }    
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
+}
+
+fn plus_one(x:Option<i32>)->Option<i32> {
+    match x {
+        None=>None,
+        Some(i)=>Some(i+1),
+        // The code in the match arm is then executed, so we add 1 to the value of i and create a new Some value with our total 6 inside.
+    }
+}
+fn main () {
+    println!("{}" ,value_in_cents(Coin::Penny));
+    println!("{}", value_in_cents(Coin::Quarter(UsStates::Alabama)));
+
+    let five = Some(5);
+    let six = plus_one(five);   // plus_one(Some(5));
+    let none = plus_one(None);
 }
